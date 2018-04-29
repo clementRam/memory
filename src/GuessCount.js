c195dd8d11345e4e66a408497b31676a243b2cdd
Clement Ramondou <cl.ramondou@gmail.com>
2018-04-29 21:01:21 +0200

    finish second part

--- src/GuessCount.js
+++ src/GuessCount.js
@@ -1,8 +1,16 @@
-
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './GuessCount.css'
 
 const GuessCount = ({guesses}) => <div className="guesses">{guesses}</div>
+
+GuessCount.propTypes = {
+    guesses: PropTypes.number.isRequired, 
+}
+
+// GuessCount.defaultProps = {
+//     guesses: 'Default value',
+// }
 
 export default GuessCount
--- src/Reset.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Reset.css';
-
-const Reset = ({reset, onClick}) => (
-    <button className="btn btn-default" onClick={ () => onClick(reset)}>Reset</button>
-);
-
-export default Reset;
